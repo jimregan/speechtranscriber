2db56efc165b10fd1b88e51dac77849e1bab3a34
Jim O'Regan <jaoregan@tcd.ie>
2020-02-01 20:16:10 +0100

    assume unstressed by default

--- src/main/java/io/github/jimregan/polishtranscriber/irishg2p/ShortVowel.java
+++ src/main/java/io/github/jimregan/polishtranscriber/irishg2p/ShortVowel.java
@@ -1,20 +1,14 @@
 package io.github.jimregan.polishtranscriber.irishg2p;
 
 public class ShortVowel extends Vowel {
-    String unstressed = "ə";
     public ShortVowel(String g, String p) {
         this.grapheme = g;
         this.stressedPhoneme = p;
+        this.unstressed = "ə";
     }
     public ShortVowel(String g, String p, String unstressed) {
         this(g, p);
         this.unstressed = unstressed;
-    }
-    String[] getUnstressed() {
-        return unstressed.split(" ");
-    }
-    String[] getStressed() {
-        return stressedPhoneme.split(" ");
     }
     @Override
     String[][] getPhonemes () {
--- src/main/java/io/github/jimregan/polishtranscriber/irishg2p/Vowel.java
+++ src/main/java/io/github/jimregan/polishtranscriber/irishg2p/Vowel.java
@@ -2,10 +2,21 @@ package io.github.jimregan.polishtranscriber.irishg2p;
 
 public class Vowel extends G2PPiece {
     String stressedPhoneme;
+    String unstressed = null;
     public boolean startSlender() {
         return Utils.startsSlenderVowel(this.getGrapheme());
     }
     public boolean endsSlender() {
         return Utils.endsSlenderVowel(this.getGrapheme());
+    }
+    String[] getUnstressed() {
+        if (this.unstressed == null) {
+            return null;
+        } else {
+            return unstressed.split(" ");
+        }
+    }
+    String[] getStressed() {
+        return stressedPhoneme.split(" ");
     }
 }
