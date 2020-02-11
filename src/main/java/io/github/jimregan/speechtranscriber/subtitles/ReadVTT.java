78d85ed469cd972b525337545de66ac7f4c48247
Jim O'Regan <jaoregan@tcd.ie>
2020-02-11 19:43:20 +0100

    sketch

--- src/main/java/io/github/jimregan/speechtranscriber/subtitles/ReadVTT.java
+++ src/main/java/io/github/jimregan/speechtranscriber/subtitles/ReadVTT.java
@@ -21,14 +21,87 @@
  */
 package io.github.jimregan.speechtranscriber.subtitles;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReadVTT {
-    public static List<SimpleTimedObject> readSimpleVTT() {
+    public static List<SimpleTimedObject> readSimpleVTT(BufferedReader br) throws IOException {
         List<SimpleTimedObject> out = new ArrayList<>();
-        int line = 0;
-
+        int lineno = 0;
+        String line;
+        Pattern p = Pattern.compile(SimpleTime.TIME_PATTERN);
+        SimpleTimedObject current = new SimpleTimedObject();
+        String tmpID = null;
+        int line_type = 0;
+        while((line = br.readLine()) != null) {
+            lineno++;
+            if(lineno == 1) {
+                if(line.startsWith("\uFEFF")) {
+                    line = line.substring(1);
+                }
+                if(!line.startsWith("WEBVTT")) {
+                    throw new IOException("Missing WEBVTT header");
+                }
+            } else {
+                if(line_type == 0) {
+                    if(line.contains("-->")) {
+                        line_type = 2;
+                        String[] parts = line.split("-->");
+                        Matcher m = p.matcher(parts[0].trim());
+                        if(!m.matches()) {
+                            throw new IOException("Error reading date in line " + lineno + ": " + line);
+                        }
+                        String end;
+                        if(parts[1].trim().contains(" ")) {
+                            String[] tmp = parts[1].trim().split(" ");
+                            end = tmp[0];
+                        } else {
+                            end = parts[1].trim();
+                        }
+                        Matcher m2 = p.matcher(end);
+                        if(!m2.matches()) {
+                            throw new IOException("Error reading date in line " + lineno + ": " + line);
+                        }
+                        current = new SimpleTimedObject(parts[0].trim(), end);
+                        if(tmpID != null) {
+                            current.setId(tmpID);
+                            tmpID = null;
+                        }
+                    } else {
+                        line_type = 1;
+                        tmpID = line.trim();
+                    }
+                } else if (line_type == 2 || line_type == 3) {
+                    if(line.trim().equals("")) {
+                        line_type = 0;
+                        out.add(current);
+                    } else {
+                        line_type = 3;
+                        current.addText(line.trim());
+                    }
+                } else if(line.trim().equals("")) {
+                    line_type = 0;
+                    out.add(current);
+                }
+            }
+        }
+        out.add(current);
         return out;
+    }
+    public static List<SimpleTimedObject> readSimpleVTT(InputStreamReader isr) throws IOException {
+        return readSimpleVTT(new BufferedReader(isr));
+    }
+    public static List<SimpleTimedObject> readSimpleVTT(FileInputStream fis) throws IOException {
+        return readSimpleVTT(new InputStreamReader(fis, StandardCharsets.UTF_8));
+    }
+    public static List<SimpleTimedObject> readSimpleVTT(File f) throws IOException {
+        return readSimpleVTT(new FileInputStream(f));
+    }
+    public static List<SimpleTimedObject> readSimpleVTT(String filename) throws IOException {
+        return readSimpleVTT(new File(filename));
     }
 }
