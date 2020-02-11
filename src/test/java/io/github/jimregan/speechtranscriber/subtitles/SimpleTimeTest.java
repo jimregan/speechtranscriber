25a52c701e7b652407d33366fb58564b008503f3
Jim O'Regan <jaoregan@tcd.ie>
2020-02-11 18:57:31 +0100

    remove unused import

--- src/test/java/io/github/jimregan/speechtranscriber/subtitles/SimpleTimeTest.java
+++ src/test/java/io/github/jimregan/speechtranscriber/subtitles/SimpleTimeTest.java
@@ -25,7 +25,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SimpleTimeTest {
