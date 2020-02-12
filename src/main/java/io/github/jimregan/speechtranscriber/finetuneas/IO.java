81deacfebe5a606c18e09167dfc143b52934ae4d
Jim O'Regan <jaoregan@tcd.ie>
2020-02-12 11:02:22 +0100

    begin reading

---
+++ src/main/java/io/github/jimregan/speechtranscriber/finetuneas/IO.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Jim O'Regan <jaoregan@tcd.ie>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package io.github.jimregan.speechtranscriber.finetuneas;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+
+public class IO {
+    public static List<Fragment> read(InputStream is)
+            throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, List<Fragment>> fragments = mapper.readValue(is, Map.class);
+
+        return fragments.get("fragments");
+    }
+    public static List<Fragment> read(String filename)
+            throws FileNotFoundException, JsonParseException, JsonMappingException, IOException {
+        return read(new FileInputStream(filename));
+    }
+}
---
+++ src/test/java/io/github/jimregan/speechtranscriber/finetuneas/IOTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Jim O'Regan <jaoregan@tcd.ie>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package io.github.jimregan.speechtranscriber.finetuneas;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class IOTest {
+
+    String sample = "{\"fragments\": [\n" +
+            "  {\n" +
+            "   \"begin\": \"0.050\",\n" +
+            "   \"end\": \"1.490\",\n" +
+            "   \"id\": \"f000001\",\n" +
+            "   \"language\": \"ga\",\n" +
+            "   \"lines\": [\n" +
+            "    \"line 1\"\n" +
+            "   ]\n" +
+            "  }, \n" +
+            "  {\n" +
+            "   \"begin\": \"1.510\",\n" +
+            "   \"end\": \"2.010\",\n" +
+            "   \"id\": \"f000002\",\n" +
+            "   \"language\": \"ga\",\n" +
+            "   \"lines\": [\n" +
+            "    \"Second line,\"\n" +
+            "   ]\n" +
+            "  }\n" +
+            " ]\n" +
+            "}";
+    @Test
+    public void testRead() throws Exception {
+        InputStream is = new ByteArrayInputStream(sample.getBytes(StandardCharsets.UTF_8));
+        List<Fragment> fragments = IO.read(is);
+    }
+}
